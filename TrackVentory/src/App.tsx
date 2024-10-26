974b3f2a3e984ce23ade3be6ed8fb4bcd0049ec1
Hafizh Kamaluddin <hafizhkamaluddin13@gmail.com>
2024-10-26 19:24:53 +0700

    y kecil

--- TrackVentory/src/App.tsx
+++ TrackVentory/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+<<<<<<< Updated upstream
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -14,6 +15,30 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+=======
+    <div className="bg-white min-h-screen">
+      <div className="sec-utama min-h-screen flex justify-center items-center">
+        <div className="sec-1 flex flex-col border-[1px] border-black items-center px-[120px] py-[50px] rounded-xl">
+          <div className="judul flex flex-col items-center gap-3">
+            <h2 className="font-bold text-[40px]">Welcome Back</h2>
+            <h2 className="font-semibold text-[20px] opacity-20">Welcome Back, Please enter your details</h2>
+          </div>
+
+          <div className="email mt-[50px] flex flex-col gap-7">
+            <input type="email" placeholder="Email" className="border-0 border-b-2 w-[350px] px-2 py-2" />
+            <input type="password" placeholder="Password" className="border-0 border-b-2 w-[350px] px-2 py-2" />
+          </div>
+
+          <div className="button flex flex-col items-center">
+            <button className="bg-black text-white w-[350px] py-[10px] rounded-xl mt-[30px]">Login</button>
+          </div>
+
+          <div className="need flex flex-row items-center gap-2 mt-[20px]">
+            <h2 className="text-black opacity-50">Need an account?</h2>
+            <button>Create an account</button>
+          </div>
+        </div>
+>>>>>>> Stashed changes
       </div>
       <h1>HAPIS ganteng</h1>
       <div className="card">
