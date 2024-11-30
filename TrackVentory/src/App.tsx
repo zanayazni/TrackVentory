243e27dbf3408719861ddf869d971369b4d8eab7
Muhammad Faried Gunawan <124080208+fariedgunawan@users.noreply.github.com>
2024-11-30 17:53:17 +0700

    update landing

--- TrackVentory/src/App.tsx
+++ TrackVentory/src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Landing from "./pages/Landing";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
       </Routes>
     </Router>
   );
---
+++ TrackVentory/src/pages/Landing.tsx
@@ -0,0 +1,10 @@
+const Landing = () => {
+  return (
+    <div className="body-landing">
+      <h2>halo landing</h2>
+      <h2 className="text-black">halo halo</h2>
+    </div>
+  );
+};
+
+export default Landing;
