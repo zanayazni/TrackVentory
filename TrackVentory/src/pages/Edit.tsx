cdf8de6790c082512b4c91d8bdf693bc2d184ae7
Daffa Prima Putra <daffapp22@gmail.com>
2025-01-03 12:49:28 +0700

    back

--- TrackVentory/src/pages/Edit.tsx
+++ TrackVentory/src/pages/Edit.tsx
@@ -7,6 +7,12 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { MdOutlineHistory } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+{
+  /* tabel area */
+}
+{
+  /* tabel area */
+}
 import { TbEdit } from "react-icons/tb";
 
 const Edit = () => {
@@ -20,57 +26,28 @@ const Edit = () => {
     jumlahStock: false,
   });
 
-  // State untuk notifikasi
-  const [showNotification, setShowNotification] = useState(false);
-
   // Fungsi untuk mengaktifkan mode edit pada field tertentu
   const toggleEdit = (field) => {
     setEditable((prev) => ({ ...prev, [field]: !prev[field] }));
   };
 
-  // Fungsi untuk handle tombol save
-  const handleSave = (e) => {
-    e.preventDefault();
-
-    // Tampilkan notifikasi
-    setShowNotification(true);
-
-    // Sembunyikan notifikasi setelah 3 detik dan arahkan ke landing page
-    setTimeout(() => {
-      setShowNotification(false);
-      navigate("/landing");
-    }, 1000);
-  };
-
   return (
     <div className="body-landing flex min-h-screen">
-      {/* side bar */}
+      {/* Sidebar */}
       <div className="sidebar min-h-screen bg-[#12376A] text-white w-[25rem] px-[20px]">
         <div className="head flex flex-row items-center justify-center mt-[20px] gap-[20px]">
           <FaDropbox className="text-[40px]" />
           <h2 className="text-[30px] font-semibold">TrackVentory</h2>
         </div>
         <div className="button-area flex flex-col justify-center mt-[60px] gap-[20px]">
-          <button
-            onClick={() => {
-              // Navigate to the current dashboard route
-              navigate("/landing");
-
-              // Trigger a page reload to refresh the dashboard
-              window.location.reload();
-            }}
-            className="button-1 flex flex-row items-center gap-[30px] px-[20px] py-[15px] bg-amber-600 rounded-2xl hover:cursor-pointer hover:shadow-sm hover:shadow-amber-500"
-          >
+          <div className="button flex flex-row items-center gap-[30px] px-[20px] py-[15px] hover:bg-amber-600 hover:rounded-2xl hover:cursor-pointer hover:text-black">
             <AiOutlineDashboard className="text-[35px]" />
             <h2 className="text-[25px]">Dashboard</h2>
-          </button>
-          <button
-            onClick={() => navigate("/History")}
-            className="button-1 flex flex-row items-center gap-[30px] px-[20px] py-[15px] hover:bg-white hover:rounded-2xl hover:cursor-pointer hover:text-black hover:shadow-md"
-          >
+          </div>
+          <div className="button flex flex-row items-center gap-[30px] px-[20px] py-[15px] hover:bg-amber-600 hover:rounded-2xl hover:cursor-pointer hover:text-black">
             <MdOutlineHistory className="text-[35px]" />
             <h2 className="text-[25px]">History</h2>
-          </button>
+          </div>
         </div>
       </div>
 
@@ -82,7 +59,7 @@ const Edit = () => {
         </div>
 
         {/* Informasi Barang */}
-        <div className="container flex justify-center mt-[10px]">
+        <div className="container flex justify-center mt-[20px]">
           <div className="box flex flex-row gap-[45px]">
             <div className="box-1 bg-white flex flex-row items-center gap-[10px] w-[350px] px-[20px] py-[20px] rounded-2xl shadow-md">
               <FaCircleArrowUp className="text-green-700 text-[60px]" />
@@ -109,11 +86,8 @@ const Edit = () => {
         </div>
 
         {/* Form Edit */}
-        <div className="container flex flex-col items-center gap-[20px] mt-[30px]">
-          <form
-            onSubmit={handleSave}
-            className="form-container bg-white w-[1142px] p-[20px] mx-[20px] rounded-2xl shadow-md"
-          >
+        <div className="container flex flex-col items-center gap-[20px] mt-[40px]">
+          <form className="form-container bg-white w-[1142px] p-[20px] mx-[20px] rounded-2xl shadow-md">
             {/* nama Barang */}
             <div className="mb-[15px]">
               <label className="block text-[18px] font-medium mb-[5px]">
@@ -122,11 +96,11 @@ const Edit = () => {
               <div className="flex items-center gap-[10px]">
                 <input
                   type="text"
-                  className="w-full px-[15px] py-[10px] border-[1px] border-gray-300 text-[#000000] rounded-lg"
+                  className="w-full px-[15px] py-[10px] bg-[#466A92] text-white rounded-lg"
                   readOnly={!editable.namaBarang}
                 />
                 <TbEdit
-                  className="text-[35px] text-black hover:text-blue-700 cursor-pointer"
+                  className="text-[24px] text-black hover:text-blue-700 cursor-pointer"
                   onClick={() => toggleEdit("namaBarang")}
                 />
               </div>
@@ -140,11 +114,11 @@ const Edit = () => {
               <div className="flex items-center gap-[10px]">
                 <input
                   type="text"
-                  className="w-full px-[15px] py-[10px] border-[1px] border-gray-300 text-[#000000] rounded-lg"
+                  className="w-full px-[15px] py-[10px] bg-[#466A92] text-white rounded-lg"
                   readOnly={!editable.kategori}
                 />
                 <TbEdit
-                  className="text-[35px] text-black hover:text-blue-700 cursor-pointer"
+                  className="text-[24px] text-black hover:text-blue-700 cursor-pointer"
                   onClick={() => toggleEdit("kategori")}
                 />
               </div>
@@ -158,11 +132,11 @@ const Edit = () => {
               <div className="flex items-center gap-[10px]">
                 <input
                   type="date"
-                  className="w-full px-[15px] py-[10px] border-[1px] border-gray-300 text-[#000000] rounded-lg"
+                  className="w-full px-[15px] py-[10px] bg-[#466A92] text-white rounded-lg"
                   readOnly={!editable.tanggalMasuk}
                 />
                 <TbEdit
-                  className="text-[35px] text-black hover:text-blue-700 cursor-pointer"
+                  className="text-[24px] text-black hover:text-blue-700 cursor-pointer"
                   onClick={() => toggleEdit("tanggalMasuk")}
                 />
               </div>
@@ -176,11 +150,11 @@ const Edit = () => {
               <div className="flex items-center gap-[10px]">
                 <input
                   type="number"
-                  className="w-full px-[15px] py-[10px] border-[1px] border-gray-300 text-[#000000] rounded-lg"
+                  className="w-full px-[15px] py-[10px] bg-[#466A92] text-white rounded-lg"
                   readOnly={!editable.jumlahStock}
                 />
                 <TbEdit
-                  className="text-[35px] text-black hover:text-blue-700 cursor-pointer"
+                  className="text-[24px] text-black hover:text-blue-700 cursor-pointer"
                   onClick={() => toggleEdit("jumlahStock")}
                 />
               </div>
@@ -189,19 +163,12 @@ const Edit = () => {
             {/* Tombol Simpan */}
             <button
               type="submit"
-              className="w-full py-[10px] bg-amber-600 text-white rounded-lg font-bold hover:bg-[#12376A]"
+              className="w-full py-[10px] bg-[#466A92] text-white rounded-lg font-bold hover:bg-[#345472]"
             >
               Save
             </button>
           </form>
         </div>
-
-        {/* Notifikasi */}
-        {showNotification && (
-          <div className="absolute top-4 right-4 bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg">
-            Data berhasil di edit!
-          </div>
-        )}
       </div>
     </div>
   );
--- TrackVentory/src/pages/Register.tsx
+++ TrackVentory/src/pages/Register.tsx
@@ -1,75 +1,15 @@
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import axios from "axios"; // Import Axios
-
 const Register = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [rePassword, setRePassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleRegister = async () => {
-    // Validasi form harus terisi semua
-    if (!username || !email || !password || !rePassword) {
-      setErrorMessage("Form masih ada yang kosong.");
-      return;
-    }
-
-    // Validasi panjang password
-    if (password.length < 8) {
-      setErrorMessage("Password harus minimal 8 karakter.");
-      return;
-    }
-
-    // Validasi password dan re-enter password
-    if (password !== rePassword) {
-      setErrorMessage("Password tidak sama dengan Re-enter Password.");
-      return;
-    }
-
-    // Reset error message jika tidak ada error
-    setErrorMessage("");
-
-    try {
-      // Mengirim data registrasi ke backend menggunakan axios
-      const response = await axios.post(
-        "http://localhost:5000/api/auth/register",
-        {
-          username,
-          email,
-          password,
-        }
-      );
-
-      // Jika registrasi berhasil, arahkan ke halaman login
-      if (response.status === 200) {
-        navigate("/"); // Atau sesuaikan dengan URL login
-      }
-    } catch (error) {
-      // Tangani error, misalnya jika email sudah terdaftar
-      if (error.response) {
-        // Respons error dari server
-        setErrorMessage("Gagal mendaftar: " + error.response.data.message);
-      } else {
-        // Error di sisi jaringan atau server tidak merespons
-        setErrorMessage("Gagal mendaftar: Terjadi kesalahan jaringan.");
-      }
-    }
-  };
-
   return (
     <div className="body-login min-h-screen w-full bg-white text-white flex">
       <div className="left-side bg-[#12376A] min-h-screen w-[760px]">
         <div className="container flex items-center justify-center min-h-screen">
           <div className="body-container flex flex-col items-start text-[60px] font-bold">
             <h2>Welcome</h2>
-            <h2 className="w-[600px] text-[30px] font-medium">
-              Create an account to join us
-            </h2>
+            <h2 className="w-[600px] text-[30px] font-medium">Create an account to join us</h2>
           </div>
         </div>
       </div>
@@ -79,59 +19,19 @@ const Register = () => {
             <h2 className="font-bold text-[40px] -ml-[175px]">Sign Up</h2>
           </div>
           <div className="input flex flex-col items-center mt-[40px] gap-[20px]">
-            <Input
-              type="text"
-              variant={"faded"}
-              label="Username"
-              placeholder="Enter your name"
-              className="w-[350px]"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <Input
-              type="email"
-              variant={"faded"}
-              label="Email"
-              placeholder="Enter your email"
-              className="w-[350px]"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              type="password"
-              variant={"faded"}
-              label="Password"
-              placeholder="Enter your password"
-              className="w-[350px]"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Input
-              type="password"
-              variant={"faded"}
-              label="Re-enter Password"
-              placeholder="Enter your Re-enter password"
-              className="w-[350px]"
-              value={rePassword}
-              onChange={(e) => setRePassword(e.target.value)}
-            />
+            <Input type="email" variant={"faded"} label="Username" placeholder="Enter your name" className="w-[350px]" />
+            <Input type="email" variant={"faded"} label="Email" placeholder="Enter your email" className="w-[350px]" />
+            <Input type="password" variant={"faded"} label="Password" placeholder="Enter your password" />
+            <Input type="password" variant={"faded"} label="Re-enter Password" placeholder="Enter your Re-enter password" />
           </div>
-          {errorMessage && (
-            <h2 className="text-red-700 mt-[10px]">{errorMessage}</h2>
-          )}
-          <Button
-            className="bg-[#12376A] text-white text-[20px] px-[30px] py-[25px] w-[350px] rounded-xl mt-[20px]"
-            onClick={handleRegister}
-          >
+          <h2 className="text-red-700 mt-[10px]">Email is already registered.</h2>
+          <Button className="bg-[#12376A] text-white text-[20px] px-[30px] py-[25px] w-[350px] rounded-xl mt-[20px]" onClick={() => navigate("/")}>
             Sign Up
           </Button>
           <h2 className="mt-[10px]">
             Already have an account?{" "}
             <span>
-              <button
-                className="text-[#12376A] font-semibold underline"
-                onClick={() => navigate("/")}
-              >
+              <button className="text-[#12376A] font-semibold underline" onClick={() => navigate("/")}>
                 Log In
               </button>
             </span>
